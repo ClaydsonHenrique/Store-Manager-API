ae8c28d41769cdd183dc1f8ab715d0cd5bb4734a
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-10-24 16:10:36 -0300

    criando rota

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+
+const { getProductController, getProductById } = require('./controllers/product.controller');
+
+app.get('/product', getProductController);
+app.get('/product/:id', getProductById);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
