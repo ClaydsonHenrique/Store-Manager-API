093efae20d9cc6891e885ddde04a71c07fe2b32f
Claydson Henrique <claydsonhenriquebs@gmail.com>
2023-10-30 19:09:51 -0300

    finalizando requisito 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,8 @@ app.use(express.json());
 
 const { getProductController, getProductById } = require('./controllers/product.controller');
 
-app.get('/product', getProductController);
-app.get('/product/:id', getProductById);
+app.get('/products', getProductController);
+app.get('/products/:id', getProductById);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
--- backend/src/models/product.model.js
+++ backend/src/models/product.model.js
@@ -1,13 +1,13 @@
-const connection = require('./connectin');
+const connection = require('./connection');
 
 const getAllProducts = async () => {
-  const query = 'SELCT * FROM StoreManager.products';
+  const query = 'SELECT * FROM StoreManager.products';
   const [resp] = await connection.execute(query);
   return resp;
 };
 
 const getAllProductById = async (id) => {
-  const query = 'SELCT * FROM StoreManager.products WHERE id = ?';
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
   const [resp] = await connection.execute(query, [id]);
   return resp;
 };
